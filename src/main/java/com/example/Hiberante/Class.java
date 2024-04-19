040456e760f858245ba0e2934ab5671f5f25d428
Jamming <j.lucreziaparrillo@gmail.com>
2024-04-19 09:36:27 +0200

    Initial commit

---
+++ src/main/java/com/example/Hiberante/Class.java
@@ -0,0 +1,19 @@
+package com.example.Hiberante;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table
+@Data
+
+public class Class {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String description;
+}
---
+++ src/main/java/com/example/Hiberante/HiberanteApplication.java
@@ -0,0 +1,13 @@
+package com.example.Hiberante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiberanteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiberanteApplication.class, args);
+	}
+
+}
